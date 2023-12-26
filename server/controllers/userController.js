1e19c2f82e9a60a1c04744d8bf15e6e5c8afa89c
Karan Pal <karanpal03040@gmail.com>
2023-12-26 18:22:35 +0530

    Created User/Admin Modifications Routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -109,16 +109,16 @@ exports.forgotPassword = CatchAsyncError(async (req, res, next) => {
 
 // Reset Password Functionality
 
-exports.resetPassword = CatchAsyncError(async (req, res, next) =>{
+exports.resetPassword = CatchAsyncError(async (req, res, next) => {
 
-     // Creating token hash
-     resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    // Creating token hash
+    resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
 
-     const user = await User.findOne({
+    const user = await User.findOne({
         resetPasswordToken,
-        resetPasswordExpire: {$gt: Date.now() },
-     })
-     if (!user) {
+        resetPasswordExpire: { $gt: Date.now() },
+    })
+    if (!user) {
         return next(new ErrorHandler('Reset Password Token is invalid or has been expired', 400));
     }
 
@@ -131,6 +131,126 @@ exports.resetPassword = CatchAsyncError(async (req, res, next) =>{
     user.resetPasswordExpire = undefined
     await user.save();
 
-    sendToken(user,200,res)
+    sendToken(user, 200, res)
 
 })
+
+
+
+//  >>>>>>>>
+
+//Get User Details
+exports.getUserDetails = CatchAsyncError(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id)
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+})
+
+// Update User Password 
+exports.updatePassword = CatchAsyncError(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id).select("+password")
+
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword)
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Old Password Is Incorrect', 400))
+    }
+
+    if (req.body.newPassword != req.body.confirmPassword) {
+        return next(new ErrorHandler('Passowrd does not match', 400))
+    }
+
+    user.password = req.body.newPassword;
+    await user.save()
+
+    sendToken(user, 200, res)
+})
+
+// Update User Profile
+exports.updateProfile = CatchAsyncError(async (req, res, next) => {
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    }
+
+    // We will cloudinary later
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    })
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+// Get All Users  --Admin
+exports.getAllUsers = CatchAsyncError(async(req,res,next)=>{
+
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users
+    })
+})
+
+// Get Single User  --Admin
+exports.getSingleUser = CatchAsyncError(async(req,res,next)=>{
+
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return next(new ErrorHandler(`User does not exits with id: ${req.params.id}`))
+    }
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
+// Update User Role --Admin
+exports.updateUserRole = CatchAsyncError(async (req, res, next) => {
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    })
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+// Delete User --Admin
+exports.deleteUser = CatchAsyncError(async (req, res, next) => {
+
+   const user = await User.findById(req.params.id);
+    //We Will remove clodinary Later
+
+    if(!user){
+        return next(new ErrorHandler(`User does not exits with id: ${req.params.id}`))
+    }
+
+    await user.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: 'User Deleted Successfully'
+    })
+})
+
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -11,12 +11,14 @@ const router = express.Router();
 
 router.route('/products').get(getAllProduct)
 
-router.route('/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct)
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct)
 
-router.route('/product/:id')
+router.route('/admin/product/:id')
     .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
-    .get(isAuthenticatedUser, getProductDetails)
+
+
+router.route('/producs/:id').get(getProductDetails)
 
 
 
