f8e72085c962eba8b4033b9b5c6a8b30bc1f3700
Karan Pal <karanpal03040@gmail.com>
2024-01-29 14:21:59 +0530

    Build For Production

--- client/src/constants/backendServer.js
+++ client/src/constants/backendServer.js
@@ -1,2 +1,2 @@
 // export const backendServer = 'http://localhost:4000'
-export const backendServer = 'https://vedicstore-server.vercel.app'
+export const backendServer = 'https://vedicstore.vercel.app'
--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const product = require('./routes/productRoute')
 const user = require('./routes/userRoutes')
 const order = require('./routes/orderRoute')
 const payment = require('./routes/paymentRoute')
+const path = require('path')
 
 const app = express();
 
@@ -25,15 +26,18 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
 
-app.get('/', (req, res) => {
-   res.send("<h1>Hello Wolrd<h1/>")
-})
-
 
 app.use('/api/v1', product)
 app.use('/api/v1', user)
 app.use('/api/v1', order)
 app.use('/api/v1', payment)
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, '../client/dist/index.html'))
+})
+
 
 // Error Handling \ Middleware
 app.use(errorMiddleware)
