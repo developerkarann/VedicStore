f3e37ad060a805501cf741b9e7780f80fb3950a6
Karan Pal <karanpal03040@gmail.com>
2024-01-29 19:52:57 +0530

    Fix cross domin coockie

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -8,6 +8,7 @@ const sendToken = (user, statusCode, res) => {
     const option = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         // httpOnly: true,
+        SameSite: 'None',
         path: "/",
     };
 
